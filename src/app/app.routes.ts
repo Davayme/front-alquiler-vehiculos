a2ef7174f6a00641e6af88767f3faf09475c5dfb
davayme <davayme@gmail.com>
2024-10-27 17:43:44 -0500

    Creacion del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { VehiclesTableComponent } from './features/Vehicle/vehicles-table/vehicles-table.component';
+import { LoginComponent } from './features/Login/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: VehiclesTableComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirección a home como ruta principal
+    { path: '**', redirectTo: '/home' } // Redirección a home para rutas no encontradas
+  ];
---
+++ src/app/features/Vehicle/Components/vehicles-table/vehicles-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicles-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicles-table.component.html',
+  styleUrl: './vehicles-table.component.css'
+})
+export class VehiclesTableComponent {
+
+}
